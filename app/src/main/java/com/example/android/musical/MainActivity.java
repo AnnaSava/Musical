87eb892cdd34dbc760fbafc0c9b543f64822dc48
Anna Savina <anna.s.savina@gmail.com>
2018-05-25 22:54:39 +0300

    Removed now playing

--- app/src/main/java/com/example/android/musical/MainActivity.java
+++ app/src/main/java/com/example/android/musical/MainActivity.java
@@ -18,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView details = (TextView) findViewById(R.id.now_playing_text);
-        details.setText("Current song");
-
         ArrayList<Song> songs = new SongRepo(getResources()).getSongs();
 
         SongAdapter adapter = new SongAdapter(this, songs);
@@ -33,11 +30,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
                 Intent songIntent = new Intent(MainActivity.this, DetailsActivity.class);
-                TextView playingView = (TextView) findViewById(R.id.now_playing_text);
 
                 Song song = (Song) adapter.getItemAtPosition(position);
 
-                playingView.setText(song.getArtistName() + " - " + song.getSongName());
                 songIntent.putExtra("EXTRA_SONG", song.getSongName());
                 songIntent.putExtra("EXTRA_ARTIST", song.getArtistName());
                 songIntent.putExtra("EXTRA_IMAGE", song.getImageResourceId());
